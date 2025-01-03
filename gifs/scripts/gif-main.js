72b24eca53f92b1e3cd8f379969a20d380d42c7b
Louie <15358089+LouieFlint@users.noreply.github.com>
2025-01-03 13:26:48 +0000

    Update gif-main.js

--- gifs/scripts/gif-main.js
+++ gifs/scripts/gif-main.js
@@ -1,17 +1,23 @@
 const allGifs = [] 
 
-async function initialiseDoc() {
-  getGifDir().then(
-    $.each(allGifs, function(i) {
-      var gif = i.path[i].split('gifs/');
-      console.log(gif);
-      $("#gif").append("<img src='./" + gif + "'/>");
-    });
-  )
+function initialiseDoc() {
+  return new Promise(async(resolve, reject)=> {
+    await getGifDir();
+    await createElements();
+    resolve();
+  });
 }
 
-function getGifDir() {
+async function getGifDir() {
   const response = await fetch("https://api.github.com/repos/LouieFlint/website/contents/gifs/src/");
   allGifs = await response.json();
   console.log(allGifs)
+}
+
+async function createElements() {
+  await $.each(allGifs, function(i) {
+      var gif = i.path[i].split('gifs/');
+      console.log(gif);
+      $("#gif").append("<img src='./" + gif + "'/>");
+  });
 }
