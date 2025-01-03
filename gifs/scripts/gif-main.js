9486f82420d9be4a0f6274e38d014cd2b96945b9
Louie <15358089+LouieFlint@users.noreply.github.com>
2025-01-03 13:01:45 +0000

    Update gif-main.js

--- gifs/scripts/gif-main.js
+++ gifs/scripts/gif-main.js
@@ -1,4 +1,4 @@
-function initialiseDoc() {
+async function initialiseDoc() {
   const response = await fetch("https://api.github.com/repos/LouieFlint/website/contents/gifs/src/");
   const todos = await response.json();
   console.log(todos);
