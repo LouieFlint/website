c64c1e8bc51d967d4bdbf210dd80529a9cc07858
Louie <15358089+LouieFlint@users.noreply.github.com>
2025-01-03 12:44:36 +0000

    Update gif-main.js

--- gifs/scripts/gif-main.js
+++ gifs/scripts/gif-main.js
@@ -1,7 +1,7 @@
 function initialiseDoc() {
   console.log('hit');
   const xhr = new XMLHttpRequest();
-  const url = "https://api.github.com/repos/louieflint/website/gifs/src/"
+  const url = "https://api.github.com/repos/LouieFlint/website/contents/gifs/src/"
   xhr.open('GET', URL, true);
   xhr.onload = function() {
       const data = JSON.parse(this.response);
